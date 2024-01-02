bdb1fecca61371270161dc76572a5bd8b0449657
B-Musick <bmuze1@gmail.com>
2024-01-01 20:48:33 -0600

    Added active button to navbar

--- src/components/TopNavBar.js
+++ src/components/TopNavBar.js
@@ -14,13 +14,13 @@ function TopNavBar(){
             key={link.label} 
             to={link.path}
             className={({isActive})=>
-                isActive ? "font-bold border-b-4 border-blue-500" : ""
+                isActive ? "font-bold border-b-4 border-blue-500 bg-sky-700 w-full text-center" : "w-full text-center"
             }
             >{link.label}</NavLink>
     })
 
     return (
-        <div className="flex justify-around">
+        <div className="flex justify-around bg-sky-900">
             {renderedLinks}
         </div>
     )
